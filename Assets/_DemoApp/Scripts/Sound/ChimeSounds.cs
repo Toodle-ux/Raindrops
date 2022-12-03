66babee49666607f338ec92f1511e165ab4f35b6
baiss33 <90279145+baiss33@users.noreply.github.com>
2022-12-03 00:13:47 -0500

    chime spawn

---
+++ Assets/_DemoApp/Scripts/Sound/ChimeSounds.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChimeSounds : MonoBehaviour
+{
+    public GameObject _ObjectToSpawn;
+    GameObject[] _FFTGameObjects;
+    public float _Spacing = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _FFTGameObjects = new GameObject[5];
+        float angleSpacing = (2f * Mathf.PI) / 5;
+         for (int i = 0; i <5; i++)
+            {
+                float angle = i * angleSpacing;
+                float x = Mathf.Sin(angle) * _Spacing;
+                float z = Mathf.Cos(angle) * _Spacing;
+
+                GameObject newFFTObject = Instantiate(_ObjectToSpawn);
+                newFFTObject.transform.SetParent(transform);
+                newFFTObject.transform.localPosition = new Vector3(x, 0, z);
+
+
+                // //---   ROTATION
+                // newFFTObject.transform.LookAt(transform.position);
+                // newFFTObject.transform.localRotation *= Quaternion.Euler(-90, 0, 0);
+
+                _FFTGameObjects[i] = newFFTObject;
+            }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
